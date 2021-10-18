c9ac4505629fe7b1245ea69ec259353b4f30fbc7
Sviridov-E <sviridoveg@mail.ru>
2021-10-18 17:03:25 +0300

    improved VCard serializer

--- src/lib/serializers.js
+++ src/lib/serializers.js
@@ -1,46 +1,36 @@
 export const serializeWifi = ({ ssid, encryption, password, hidden }) =>
   `WIFI:S:${ssid};T:${encryption};P:${password};H:${hidden};`;
 
-export const serializeVCard = ({
-  firstname,
-  lastname,
-  org,
-  title,
-  tel,
-  url,
-  email,
-}) => {
-  let name = lastname ? `N:${lastname};${firstname}\n` : `FN:${firstname}\n`;
-  org = org ? `ORG: ${org}\n` : "";
-  title = title ? `TITLE: ${title}\n` : "";
-  tel = tel ? `TEL: ${tel}\n` : "";
-  url = url ? `URL: ${url}\n` : "";
-  email = email ? `EMAIL: ${email}\n` : "";
-
-  return (
-    "BEGIN:VCARD\n" + name + org + title + tel + url + email + "END:VCARD;"
-  );
-};
-
-export const serializePhone = ({ tel }) => `tel:${tel}`;
-
 export const serializeLink = ({ link }) => {
-  let result = "";
+  let result = '';
   let match = link.match(/https?:\/\//i);
   if (!(match && !match.index)) {
-    result = "https://";
+    result = 'https://';
   }
   return result + link;
 };
 
+export const serializeVCard = ({ firstname, lastname, org, title, tel, url, email }) => {
+  let name = lastname ? `N:${lastname};${firstname}\n` : `FN:${firstname}\n`;
+  org = org ? `ORG:${org}\n` : '';
+  title = title ? `TITLE:${title}\n` : '';
+  tel = tel ? `TEL:${tel}\n` : '';
+  url = url ? `URL:${serializeLink({ link: url })}\n` : '';
+  email = email ? `EMAIL:${email}\n` : '';
+
+  return 'BEGIN:VCARD\n' + name + org + title + tel + url + email + 'END:VCARD;';
+};
+
+export const serializePhone = ({ tel }) => `tel:${tel}`;
+
 let serializers = {
-    wifi: serializeWifi,
-    vcard: serializeVCard,
-    phone: serializePhone,
-    link: serializeLink,
-    text: i => i
+  wifi: serializeWifi,
+  vcard: serializeVCard,
+  phone: serializePhone,
+  link: serializeLink,
+  text: (i) => i,
 };
 
 export default function matchSerializerO(type) {
-    return serializers[type.toLowerCase()]
+  return serializers[type.toLowerCase()];
 }
